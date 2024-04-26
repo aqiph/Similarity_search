0bad6567324723263c870a641ecbca5d1e58a877
aqiph <aqiph818@gmail.com>
2024-04-26 11:21:39 +0800

    Modify get_nth_analog

--- run_similarity_search.py
+++ run_similarity_search.py
@@ -6,8 +6,14 @@ Created on Thu Mar 30 11:14:00 2023
 @author: guohan
 """
 
+import sys, time, os
+path_list = sys.path
+module_path = '/Users/guohan/Documents/Codes/Similarity_search'
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    print('Add module path')
 
-from similarity_search import similarity_search_single_ref, similarity_search_multiple_ref, get_most_similar_analog, plot_distribution
+from similarity_search import similarity_search_single_ref, similarity_search_multiple_ref, get_nth_analog, plot_distribution
 
 
 def run_similarity_search_single_ref():
@@ -16,8 +22,8 @@ def run_similarity_search_single_ref():
     """
     input_file = 'tests/library.csv'
     smiles_column_name = 'Cleaned_SMILES'
-    # smiles_ref = 'N#CC1(Oc2ccc(-c3ccccc3)cc2)CC2(CCN(C(=O)Nc3ccccc3)C2)C1'
-    smiles_ref = 'C1=CC=CC=C1'
+    smiles_ref = 'N#CC1(Oc2ccc(-c3ccccc3)cc2)CC2(CCN(C(=O)Nc3ccccc3)C2)C1'
+    # smiles_ref = 'C1=CC=CC=C1'
 
     output_folder = 'tests/similarity_search_results'
     output_file = 'output.csv'
@@ -26,9 +32,9 @@ def run_similarity_search_single_ref():
     ### Method: 'fingerprint' ###
     # similarity_search_single_ref(input_file, smiles_column_name, smiles_ref, method='fingerprint', similarity_cutoff=0.3, output_folder=output_folder, output_file=output_file, output_option=output_option)
     ### Method: 'mcs' ###
-    # similarity_search_single_ref(input_file, smiles_column_name, smiles_ref, method='mcs', mcs_match='exact', similarity_cutoff=0.3, output_folder=output_folder, output_file=output_file, output_option=output_option)
+    similarity_search_single_ref(input_file, smiles_column_name, smiles_ref, method='mcs', mcs_match='exact', similarity_cutoff=0.3, output_folder=output_folder, output_file=output_file, output_option=output_option)
     ### Method: 'substructure' ###
-    similarity_search_single_ref(input_file, smiles_column_name, smiles_ref, method='substructure', substructure_method='SMARTS', output_folder=output_folder, output_file=output_file, output_option=output_option)
+    # similarity_search_single_ref(input_file, smiles_column_name, smiles_ref, method='substructure', substructure_method='SMARTS', output_folder=output_folder, output_file=output_file, output_option=output_option)
 
 
 def run_similarity_search_multiple_ref():
@@ -60,7 +66,7 @@ def run_get_most_similar_analog():
     """
     input_file = 'tests/reference_cmps.csv'
     analogs_dir = 'tests/similarity_search_results'
-    get_most_similar_analog(input_file, analogs_dir, n=1)
+    get_nth_analog(input_file, analogs_dir, rank=1)
 
 
 def run_plot_distribution():
@@ -70,7 +76,7 @@ def run_plot_distribution():
 
 
 if __name__ == '__main__':
-    run_similarity_search_single_ref()
-    # run_similarity_search_multiple_ref()
+    # run_similarity_search_single_ref()
+    run_similarity_search_multiple_ref()
     # run_get_most_similar_analog()
     # run_plot_distribution()
