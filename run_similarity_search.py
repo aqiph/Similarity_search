aec82e0b97fbc8b514dc7bd2685b8b477834ee94
aqiph <aqiph818@gmail.com>
2025-08-25 17:31:57 +0800

    fix bug

--- run_similarity_search.py
+++ run_similarity_search.py
@@ -13,32 +13,10 @@ if module_path not in sys.path:
     sys.path.append(module_path)
     print('Add module path')
 
-from similarity_search import similarity_search_single_query, similarity_search_multiple_query, select_analogs, plot_distribution
+from similarity_search import similarity_search_multiple_query, select_analogs, plot_distribution
 
 
-def run_similarity_search_single_query():
-    """
-    Perform similarity search for single query SMILES
-    """
-    input_file_library = 'tests/library.csv'
-    # input_file_library = '/Users/guohan/Documents/Projects/Datasets/HTS/Combination/forGNN/HTS_forGNN_446663.csv'
-    smiles_column_name = 'Cleaned_SMILES'
-    smiles_query = 'N#CC1(Oc2ccc(-c3ccccc3)cc2)CC2(CCN(C(=O)Nc3ccccc3)C2)C1'
-    # smiles_query = 'C1=CC=CC=C1'
-
-    output_folder = 'tests/similarity_search_results'
-    output_file = 'output.csv'
-    output_option = 'satisfied'
-
-    ### Method: 'fingerprint' ###
-    # similarity_search_single_query(input_file_library, smiles_column_name, smiles_query, method='fingerprint', similarity_cutoff=0.3, output_folder=output_folder, output_file=output_file, output_option=output_option)
-    ### Method: 'mcs' ###
-    similarity_search_single_query(input_file_library, smiles_column_name, smiles_query, method='mcs', mcs_match='exact', similarity_cutoff=0.3, output_folder=output_folder, output_file=output_file, output_option=output_option)
-    ### Method: 'substructure' ###
-    # similarity_search_single_query(input_file_library, smiles_column_name, smiles_query, method='substructure', substructure_method='SMARTS', output_folder=output_folder, output_file=output_file, output_option=output_option)
-
-
-def run_similarity_search_multiple_query():
+def run_similarity_search_multiple_query(method = 'fingerprint'):
     """
     Perform similarity search for multiple query SMILES in input_file_query
     """
@@ -52,14 +30,17 @@ def run_similarity_search_multiple_query():
     output_option = 'satisfied'
 
     ### Method: 'fingerprint' ###
-    # similarity_search_multiple_query(input_file_library, input_file_query, id_column_name_query, smiles_column_name_query,
-    #                                method='fingerprint', similarity_cutoff=0.3, output_folder=output_folder, output_option=output_option)
+    if method == 'fingerprint':
+        similarity_search_multiple_query(input_file_library, input_file_query, method='fingerprint',
+                                         similarity_cutoff=0.3, output_folder=output_folder, output_option=output_option)
     ### Method: 'mcs' ###
-    similarity_search_multiple_query(input_file_library, input_file_query, id_column_name_query, smiles_column_name_query,
-                                   method='mcs', mcs_match='exact', similarity_cutoff=0.3, output_folder=output_folder, output_option=output_option)
+    elif method == 'mcs':
+        similarity_search_multiple_query(input_file_library, input_file_query, method='mcs',
+                                         mcs_match='exact', similarity_cutoff=0.3, output_folder=output_folder, output_option=output_option)
     ### Method: 'substructure' ###
-    # similarity_search_multiple_query(input_file_library, input_file_query, id_column_name_query, smiles_column_name_query,
-    #                                method='substructure', substructure_method='SMARTS',output_folder=output_folder, output_option=output_option)
+    elif method == 'substructure':
+        similarity_search_multiple_query(input_file_library, input_file_query, method='substructure',
+                                         substructure_method='SMARTS',output_folder=output_folder, output_option=output_option)
 
 
 def run_select_analogs():
@@ -89,7 +70,6 @@ def run_plot_distribution():
 
 
 if __name__ == '__main__':
-    # run_similarity_search_single_query()
     # run_similarity_search_multiple_query()
     run_select_analogs()
     # run_plot_distribution()
